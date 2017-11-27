7ee79ca085f8e0c9ee02c5f08111437549192815
Jeff Senn <senn@maya.com>
2017-11-27 09:38:03 -0500

    fixes

--- vsmf.js
+++ vsmf.js
@@ -1,19 +1,23 @@
-var Immutable = require('immutable');
+const Immutable = require('immutable');
 
 module.exports = function() {
     function get(a) {
-        return Immutable.Map.isMap(a) ? a.getIn(arguments.slice(1)) :
+        //this is not very efficient
+        return Immutable.Map.isMap(a) ? a.getIn(Array.prototype.slice.call(arguments,1)) :
                undefined;
     }
     function getIn(a,b) {
         return Immutable.Map.isMap(a) ? a.getIn(b) : undefined;
     }
     function isSpecial(a) {
-        if(a.getIn) return a.getIn(["",0]);
+        if(a.getIn && a.size == 1) return a.getIn(["",0]);
         if(a[""] && Object.keys(a).length == 1)
             return a[""][0];
         return undefined;
     };
+    function asString(a) {
+        return (typeof(a) == "string") ? a : (""+a);
+    }
     var vsmf = {
         isEForm: function(a) {
             return Immutable.Map.isMap(a) && !isSpecial(a);
@@ -31,7 +35,7 @@ module.exports = function() {
             return (typeof(a) == "number");
         },
         toUUID: function(a) {
-            return {"":["UUID",a]};
+            return Immutable.fromJS({"":["UUID",asString(a)]});
         },
         asUUIDString: function(a) {
             return (isSpecial(a) == "UUID") ? (a.getIn ? a.getIn(["",1]) : a[""][1]) : undefined;
@@ -39,14 +43,12 @@ module.exports = function() {
         asNumber: function(a) {
             return x === null ? NaN : +a; //was: Number(a);
         },
-        asString: function(a) {
-            return (typeof(a) == "string") ? a : (""+a);
-        },
+        asString: asString,
+        getIn: getIn,
+        get: get,
+        isSpecial: isSpecial,
+        Immutable: Immutable
     }
-    vsmf.getIn = getIn;
-    vsmf.get = get;
-    vsmf.isSpecial = isSpecial;
-    vsmf.Immutable = Immutable
     return vsmf;
 }();
 
